f836ddae0a4649d3e701819e4a5096d0bcb36272
yunglleung1 <yungl.leung1@gmail.com>
2018-09-26 13:23:54 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,14 @@
-// Code SimplerComponent Here
+import React, {Component} from 'react'
+
+//create stateless functional component SimplerComponent
+const SimplerComponent = (props) => {
+  // console.log(props.handleClick)
+  //render <div> to page that contains text: "I am just happy"
+  return (
+    <div onClick={() => {props.handleClick()}}>"I am just happy"</div>
+  )
+}
+
+export default SimplerComponent
+
+//receives property handleClick that performs some sort of action in response to a click
